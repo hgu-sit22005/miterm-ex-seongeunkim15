4f396212944b08e6b4b5c54a991ff369257c1bb2
seongeunkim15 <21500082@handong.edu>
2019-10-14 00:18:38 +0900

    practice

--- Problem03/problem03.cpp
+++ Problem03/problem03.cpp
@@ -7,6 +7,31 @@ int main(int argc, char** argv)
 	char buff[1000];
     std::cout << "Enter a string " << std::endl;
     std::cout << ">>";
+    std::cin >> buff;
+    
+    char buff2[101];
+	int idx = 0;
+
+	for(int i = 0; i < strlen(buff); i++)
+	{
+		if(buff[i] != 'a')
+			if(buff[i] != 'A')
+				if(buff[i] != 'e')
+					if(buff[i] != 'E')
+						if(buff[i] != 'i')
+							if(buff[i] != 'I')
+								if(buff[i] != 'o')
+									if(buff[i] != 'O')
+										if(buff[i] != 'u')
+											if(buff[i] != 'U')
+											{
+												buff2[idx] = buff[i];
+												idx += 1;
+											}
+
+	}
+	buff2[idx] = '\0';
+	std::cout << buff2 << std::endl;
 
 
 
